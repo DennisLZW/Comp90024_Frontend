99f32cdbb70cdaf87e2d42bbf44f56abab5a8597
DennisLZW <110036584+DennisLZW@users.noreply.github.com>
2023-05-16 16:49:40 +1000

    Home page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Link, Switch, Routes } from "react-router-dom";
-import Introduction from './components/Introduction';
-
-
 //load components
 //import TableMap from './components/TableMap';
+import Dashboard from './components/Dashboard';
+import Home from './components/Home';
 
-
+import {
+  MapContainer,
+  TileLayer
+} from 'react-leaflet';
 
 function App() {
   return (
-    
-      <div className="App" >
-      <h1 style={{fontSize: '50px'}}>House Rental</h1>
-      <Introduction/>
-      </div>
-    
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+      
   );
 }
 
